240e5fd6c1c9862cc1607807b63b178ba8e608f5
ojdunn <dunnow@mail.gvsu.edu>
2022-02-26 06:42:15 -0500

    updated several components to latest versions and some changes to properties and manifest to allow it: api 32, kotlin, gradle

--- app/src/main/java/com/owenjdunn/game2048/TitleScreen.kt
+++ app/src/main/java/com/owenjdunn/game2048/TitleScreen.kt
@@ -3,9 +3,11 @@ package com.owenjdunn.game2048
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TitleScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.title_screen)
     }
+
+
 }
